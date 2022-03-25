53fac859bf225c9dcd1d58bc11e26a539b391ec6
jiriks74 <jiri.stefka.js@gmail.com>
2022-03-25 13:05:25 +0100

    One commit

---
+++ palindrom/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+
+namespace palindrom
+{
+    class palindrom
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            string response = null;
+            while (true)
+            {
+                Console.Write("Do you want to choose a file [f] or type into the command line [t]?: ");
+                response = Console.ReadLine();
+                Console.Clear();
+                if(response == "f" | response == "t")break;
+            }
+
+            string[] input = new string[1];
+            if (response == "f")
+            {
+                string filePath = null;
+                bool first = true;
+                while (!File.Exists(filePath))
+                {
+                    if (!File.Exists(filePath) && !first)
+                    {
+                        Console.Write("Please input valid file: ");
+                    }
+                    Console.Write("Please write the file name: ");
+                    filePath = Console.ReadLine();
+                    Console.Clear();
+                    first = false;
+                }
+                input = File.ReadAllLines(filePath);
+            }
+            else
+            {
+                while (true)
+                {
+                    Console.Write("Please insert word or sentence (mini 5 characters, max 20 characters): ");
+                    input[0] = Console.ReadLine();
+                    Console.Clear();
+                    if(input[0].Length > 4)break;
+                }
+            }
+
+            foreach (var line in input)
+            {
+                string inputLine = line;
+                var charsToRemove = new string[] { "@", ",", ".", ";", "'", " " };
+                foreach (var c in charsToRemove)
+                {
+                    inputLine = inputLine.Replace(c, string.Empty);
+                }
+
+                string testIfPalindrom = null;
+
+                for (int i = inputLine.Length - 1; i >= 0; i--)
+                {
+                    testIfPalindrom += inputLine[i];
+                }
+
+                if(response == "f"){
+                if (inputLine == testIfPalindrom)
+                {
+                    Console.WriteLine(inputLine);
+                }}
+                else{
+                    Console.WriteLine("Palindrom");
+                    Console.WriteLine(input[0].Length + " characters long.");
+                }
+                // else
+                // {
+                //     Console.WriteLine("Not a palindrom.");
+                // }
+            }
+        }
+    }
+}
--- semafor/ui/MainWindow.cs
+++ semafor/ui/MainWindow.cs
@@ -1,7 +1,7 @@
 using System;
 using Gtk;
 using Cairo;
-using System.Threading;
+using System.Timers;
 
 namespace semafor.ui.MainWindow
 {
@@ -10,24 +10,44 @@ namespace semafor.ui.MainWindow
         // Value to track playback state
         private bool _play = true;
 
+        // Values for switching timings - how long will each be turn on for
+        private int carRedTime = 5;
+        private int carOrangeTime = 2;
+        private int carGreenTime = 10;
+        private int elapsedSeconds = 0; // Seconds elapsed from last switch
+
+        // Value to track lights for car
+        private bool carRedLight = true;
+        private bool carGreenLight = false;
+
         // Initialize window builder (interface between the frontend and backend)
         private Builder _builder;
 
         // Get all the object in the window
         [Builder.Object]
-        private DrawingArea pedestRed = null;
+        private DrawingArea pedestRed;
 
         [Builder.Object]
-        private DrawingArea pedestGreen = null;
+        private DrawingArea pedestGreen;
 
         [Builder.Object]
-        private Grid grid = null;
+        private DrawingArea carRed;
+        [Builder.Object]
+        private DrawingArea carOrange;
+        [Builder.Object]
+        private DrawingArea carGreen;
+
+
+        [Builder.Object]
+        private Button playButton = null;
+        [Builder.Object]
+        private Window mainWindow;
 
 
         public static MainWindow Create()
         {
             Builder builder = new Builder(null, "semafor.ui.MainWindow.glade", null);
-            return new MainWindow(builder, builder.GetObject("MainWindow").Handle);
+            return new MainWindow(builder, builder.GetObject("mainWindow").Handle);
         }
 
         protected MainWindow(Builder builder, IntPtr handle) : base(handle)
@@ -35,12 +55,13 @@ namespace semafor.ui.MainWindow
             _builder = builder;
             builder.Autoconnect(this);
             SetupHandlers();
-            //draw();
+            InitTimer();
         }
 
         protected void SetupHandlers()
         {
             DeleteEvent += OnLocalDeleteEvent;
+            // playButton.Activate += playButtonClick();
         }
 
         protected void OnLocalDeleteEvent(object sender, DeleteEventArgs a)
@@ -49,17 +70,60 @@ namespace semafor.ui.MainWindow
             a.RetVal = true;
         }
 
-        private void SetupTimer()
+        private void InitLights(object source, ElapsedEventArgs e){
+            drawRed(carRed);
+            drawGreen(pedestGreen);
+
+            renderColors.Enabled = false;
+            timer.Enabled = true;
+        }
+
+        private Timer timer;
+        private Timer renderColors;
+        private void InitTimer()
         {
-            // Create an AutoResetEvent to signal the timeout threshold in the
-            // timer callback has been reached.
+            // Create a timer
+            timer = new Timer();
+            // Tell the timer what to do when it elapses
+            timer.Elapsed += new ElapsedEventHandler(updateTime);
+            // Set it to go off every five seconds
+            timer.Interval = 1000;
+            // And stop it
+            timer.Enabled = false;
 
-            // Create a timer that invokes CheckStatus after one second, 
-            // and every 1/4 second thereafter.
-            // var drawTimer = new Timer(draw(), 100);
+            renderColors = new Timer();
+            renderColors.Interval = 1500;
+            renderColors.Elapsed += new ElapsedEventHandler(InitLights);
+            renderColors.Enabled = true;
+        }
 
-
-            Console.WriteLine("\nDestroying timer.");
+        private void updateTime(object source, ElapsedEventArgs e)
+        {
+            if(carRedLight && elapsedSeconds >= carRedTime){
+                carRedLight = false;
+                carGreenLight = false;
+                elapsedSeconds = 0;
+                draw();
+                return;
+            }
+            else if (!carRedLight && !carGreenLight && elapsedSeconds >= carOrangeTime){
+                carRedLight = false;
+                carGreenLight = true;
+                elapsedSeconds = 0;
+                draw();
+                return;
+            }
+            else if(carGreenLight && elapsedSeconds >= carGreenTime){
+                carRedLight = true;
+                carGreenLight = false;
+                elapsedSeconds = 0;
+                draw();
+                return;
+            }
+            else{
+                elapsedSeconds += 1;
+                return;
+            }
         }
 
         /// <summary>
@@ -67,20 +131,127 @@ namespace semafor.ui.MainWindow
         /// </summary>
         private void draw()
         {
-            Cairo.Context cr = Gdk.CairoHelper.Create(GdkWindow);
+            if (carRedLight)
+            {
+                //carRed;
+                drawRed(carRed);
+                drawBlank(carOrange);
+                drawBlank(carGreen);
+
+                drawBlank(pedestRed);
+                //pedestGreen;
+                drawGreen(pedestGreen);
+                return;
+            }
+            else
+            {
+                //pedestRed;
+                drawRed(pedestRed);
+                drawBlank(pedestGreen);
+
+                if (carGreenLight)
+                {
+                    //carGreen;
+                    drawGreen(carGreen);
+
+                    drawBlank(carRed);
+                    drawBlank(carOrange);
+                    return;
+                }
+                else
+                {
+                    //carRed;
+                    drawRed(carRed);
+                    //carOrange;
+                    drawOrange(carOrange);
+
+                    drawBlank(carGreen);
+                    return;
+                }
+            }
+        }
+
+        private void drawRed(DrawingArea target)
+        {
+            int width, height;
+            Cairo.Context cr = Gdk.CairoHelper.Create(target.GdkWindow);
 
             cr.LineWidth = 9;
-            cr.SetSourceRGB(0.7, 0.2, 0.0);
+            cr.SetSourceRGB(1, 0, 0);
 
-            int width, height;
-            width = Allocation.Width;
-            height = Allocation.Height;
+            width = carRed.AllocatedWidth;
+            height = carRed.AllocatedHeight;
 
             cr.Translate(width / 2, height / 2);
             cr.Arc(0, 0, (width < height ? width : height) / 2 - 10, 0, 2 * Math.PI);
             cr.StrokePreserve();
 
-            cr.SetSourceRGB(0.3, 0.4, 0.6);
+            cr.SetSourceRGB(1, 0, 0);
+            cr.Fill();
+
+            ((IDisposable)cr.Target).Dispose();
+            ((IDisposable)cr).Dispose();
+        }
+
+        private  void drawGreen(DrawingArea target)
+        {
+            int width, height;
+            Cairo.Context cr = Gdk.CairoHelper.Create(target.GdkWindow);
+
+            cr.LineWidth = 9;
+            cr.SetSourceRGB(0, 1, 0);
+
+            width = carRed.AllocatedWidth;
+            height = carRed.AllocatedHeight;
+
+            cr.Translate(width / 2, height / 2);
+            cr.Arc(0, 0, (width < height ? width : height) / 2 - 10, 0, 2 * Math.PI);
+            cr.StrokePreserve();
+
+            cr.SetSourceRGB(0, 1, 0);
+            cr.Fill();
+
+            ((IDisposable)cr.Target).Dispose();
+            ((IDisposable)cr).Dispose();
+        }
+
+        private  void drawOrange(Gtk.DrawingArea target)
+        {
+            int width, height;
+            Cairo.Context cr = Gdk.CairoHelper.Create(target.GdkWindow);
+
+            cr.LineWidth = 9;
+            cr.SetSourceRGB(1, 0.75, 0);
+
+            width = carRed.AllocatedWidth;
+            height = carRed.AllocatedHeight;
+
+            cr.Translate(width / 2, height / 2);
+            cr.Arc(0, 0, (width < height ? width : height) / 2 - 10, 0, 2 * Math.PI);
+            cr.StrokePreserve();
+
+            cr.SetSourceRGB(1, 0.75, 0);
+            cr.Fill();
+
+            ((IDisposable)cr.Target).Dispose();
+            ((IDisposable)cr).Dispose();
+        }
+        private  void drawBlank(DrawingArea target)
+        {
+            int width, height;
+            Cairo.Context cr = Gdk.CairoHelper.Create(target.GdkWindow);
+
+            cr.LineWidth = 9;
+            cr.SetSourceRGB(0, 0, 0);
+
+            width = carRed.AllocatedWidth;
+            height = carRed.AllocatedHeight;
+
+            cr.Translate(width / 2, height / 2);
+            cr.Arc(0, 0, (width < height ? width : height) / 2 - 10, 0, 2 * Math.PI);
+            cr.StrokePreserve();
+
+            cr.SetSourceRGB(0, 0, 0);
             cr.Fill();
 
             ((IDisposable)cr.Target).Dispose();
